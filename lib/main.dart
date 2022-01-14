1a88fe9d9405990131a6b031733c386f90e4389f
moreauc-coder <GGu_est@outlook.fr>
2022-01-14 14:35:17 +0100

    formualire (main)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get.dart';
 import 'package:projet_alexis/presentation/core/constants/colors.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -31,27 +32,227 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Get.theme.secondaryHeaderColor,
       appBar: AppBar(
-        title: const Center(child: Text("Mon app"),),
+        title: const Center(
+          child: Text("Mon app"),
+        ),
         backgroundColor: kGrey6,
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // List.generate(
-            //   controller.myApps.length,
-            //       (index) =>
-            //       _buildAppItem(controller.myApps[index], context),
-            // ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                height: context.height / 2.3,
+                child: Card(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              "Information",
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 20.0),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 16),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                constraints: BoxConstraints(
+                                  minWidth: 100,
+                                  maxWidth: 150,
+                                ),
+                                border: OutlineInputBorder(),
+                                hintText: 'Nom',
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                constraints: BoxConstraints(
+                                  minWidth: 100,
+                                  maxWidth: 150,
+                                ),
+                                border: OutlineInputBorder(),
+                                hintText: 'Prenom',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                constraints: BoxConstraints(
+                                  minWidth: 100,
+                                  maxWidth: 300,
+                                ),
+                                border: OutlineInputBorder(),
+                                hintText: 'Société',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                constraints: BoxConstraints(
+                                  minWidth: 100,
+                                  maxWidth: 300,
+                                ),
+                                border: OutlineInputBorder(),
+                                hintText: 'poste',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                height: context.height / 1.8,
+                child: Card(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              "Coordonnées",
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 20.0),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.phone,
+                                ),
+                                constraints: BoxConstraints(
+                                  minWidth: 100,
+                                  maxWidth: 300,
+                                ),
+                                border: OutlineInputBorder(),
+                                hintText: 'Téléphone',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.phone_iphone,
+                                ),
+                                constraints: BoxConstraints(
+                                  minWidth: 100,
+                                  maxWidth: 300,
+                                ),
+                                border: OutlineInputBorder(),
+                                hintText: 'Portable',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.call_end,
+                                ),
+                                constraints: BoxConstraints(
+                                  minWidth: 100,
+                                  maxWidth: 300,
+                                ),
+                                border: OutlineInputBorder(),
+                                hintText: 'Fax',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.email,
+                                ),
+                                constraints: BoxConstraints(
+                                  minWidth: 100,
+                                  maxWidth: 300,
+                                ),
+                                border: OutlineInputBorder(),
+                                hintText: 'Email',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Increment',
